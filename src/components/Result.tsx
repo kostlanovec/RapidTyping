9ab8a24c59bc867cf4b70f3f85fcb0cd9a715765
Jan S. Kostlán <jan.kostlan.021@pslib.cz>
2024-01-25 19:43:28 +0100

    přidání dalšího result

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -4,7 +4,9 @@ import { ResultsProps } from '../types/type';
 
 
 const Result: React.FC<ResultsProps> = ({ toMainMenu }) => {
-  const [ {typingSpeed, numberMistakes, timeResult}  ] = useContext(PlayingContext);
+  const [ {typingSpeed, numberMistakes, timeResult, typingText, time}  ] = useContext(PlayingContext);
+  const netWordsPerMinute = Math.round((typingText.length - numberMistakes) / time * 60)
+
   return (
     <div>
       <h2>Tady máš výsledek</h2>
@@ -12,6 +14,7 @@ const Result: React.FC<ResultsProps> = ({ toMainMenu }) => {
       <p>{typingSpeed / 5} počet slov za minutu</p>
       <p>{numberMistakes} počet chyb</p>
       <p>{timeResult} čas v milisekundách</p>
+      <p>{netWordsPerMinute} Čisté úhozy za minutu</p>
       <button onClick={toMainMenu}>Hlavní menu</button>
     </div>
   );
